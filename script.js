acc26e10bf3035314434d2f14eb299bcafa77de1
tiszabalazsnandor <tiszab@kkszki.hu>
2024-05-21 14:04:13 +0200

    Egyedüli megjelenítés

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
+ let id = 0;
+let adatok = []
+
 fetch("https://pizza.kando-dev.eu/Pizza")
 .then(function (res) {
     return res.json();
 })
-.then(function (datas) {
-    console.log(datas);
+.then(function (res) {
+    console.log(res);
     let content = document.getElementById("pizda");
-    for (const data of datas) {
+    for (const data of res) {
+        id = data.id;
         content.innerHTML += `<tr class="zipda">
         <td><h3>${data.name}
             <img src="${data.kepURL}" alt="${data.name}" width="300">
         </h3></td>
         <td><h3>Az űr legfrissebb gyümölcsei préselve</h3></td>
         <td><h3>5 GP</h3></td>
-    </tr>`
+    </tr>
+    <button onclick="proba(${id})">Részletek</button>`
+    
     }
+    
 })
+
+function proba(id) {
+    fetch("https://pizza.kando-dev.eu/Pizza/"+id)
+.then(function (res) {
+    return res.json();
+})
+.then(function (res) {
+    console.log(res);
+    let content = document.getElementById("pizda");
+    {
+        content.innerHTML = `<tr class="zipda">
+        <td><h3>${res.name}
+            <img src="${res.kepURL}" alt="${res.name}" width="300">
+        </h3></td>
+        <td><h3>Az űr legfrissebb gyümölcsei préselve</h3></td>
+        <td><h3>5 GP</h3></td>
+    </tr>
+    `}})
+}
